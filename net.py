e884c87832f18535698ce362091aad04e27ef077
Will Loughlin <william.loughlin@stonybrook.edu>
2017-11-10 13:16:02 -0500

    backprop?

--- net.py
+++ net.py
@@ -9,11 +9,19 @@ FINAL_SIZE = 10
 
 
 def sigmoid(x):
-	return exp(x)/(1+exp(x))
+	return 1/(1+np.exp(-x))
+
+def d_sigmoid(x):
+	return sigmoid(x)*(1-sigmoid(x))
 
 
 def cost(result, target):
-	target_array = np.zeroes((FINAL_SIZE, 1))
+	target_array = np.zeros((FINAL_SIZE, 1))
+	target_array[target][0] = 1
+	cost_array = (target_array - result)**2
+	d_cost_array = 2*(target_array-result)
+	cost = np.sum(cost_array)
+	return (cost_array, d_cost_array, cost)
 
 
 class net(object):
@@ -26,10 +34,58 @@ class net(object):
 		layer_size.append(FINAL_SIZE)
 		self.w_and_b = [(10*(np.random.rand(
 			layer_size[x+1], layer_size[x])-0.5), 10*(np.random.rand(
-			layer_size[x], 1)-0.5)) for x in range(NUM_HL+1)]
+			layer_size[x+1], 1)-0.5)) for x in range(NUM_HL+1)]
 
-	def feed_foward(x):
+	def feed_foward(self, x):
+		activations = []
 		activation = x
 		for (weight, bias) in self.w_and_b:
-			activation = sigmoid(weight*activation + bias)
-		return activation
+			result = weight.dot(activation) + bias
+			activation = sigmoid(result)
+			[(activation, result)] + activations
+		return activations
+
+	def train():
+		batch = self.data_set.make_minibatches(1000)
+		size = self.data_set.size/1000
+		
+		for (picture, label) in batch:
+
+
+
+	def backprop(activations, cost, label)
+
+		d_cost = 2*(act_l[0]-cost[0])
+		for l in range(NUM_HL+1):
+			act_l = activations[l]
+			act_l_minus = activations[l+1]
+			# calculate changes to weights and biases in current layer
+			sp_z = d_sigmoid(act_l[1])
+			change_b = sp_z*d_cost
+			change_w = (change_b).dot(act_l_minus[0])
+			w_b = self.w_and_b[NUM_HL-l]
+			#calculate cost for next layer
+			w_trans = np.transpose(w_b[0])
+			d_cost = w_trans.dot(change_b)
+			w_b[0] += change_w
+			w_b[1] += change_b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- processor.py
+++ processor.py
@@ -22,15 +22,14 @@ class processor(object):
 		self.rows = struct.unpack('>i', self.img_file.read(4))[0]
 		self.cols = struct.unpack('>i', self.img_file.read(4))[0]
 		self.img_size = self.rows * self.cols
-
 		pixels = np.fromfile(self.img_file, dtype=np.uint8)
-		pixels = pixels[IMG_OFFSET:]
 		pictures = [pixels[self.img_size * x : self.img_size * (x+1)] for x in range(self.size)]
 		labels = np.fromfile(self.lbl_file, dtype=np.uint8)
-		labels = labels[LBL_OFFSET-4]
-		self.data = [(pictures[x], labels[x]) for x in range(self.size)]
+		labels = labels[LBL_OFFSET-4:]
+		self.data = [(np.reshape(pictures[x], (self.img_size, 1)), labels[x]) for x in range(self.size)]
 
-		def make_minibatchs(self, num_batches):
-			batches = [[] for i in range(num_batches)]
-			for x in self.data:
-				batches[random.nextint(0, num_batches-1)].append(x)
+	def make_minibatches(self, num_batches):
+		batches = [[] for i in range(num_batches)]
+		for x in self.data:
+			batches[random.randint(0, num_batches-1)].append(x)
+		return batches
